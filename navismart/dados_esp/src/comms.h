5fc2b3bc80b59c3995838feb66c070f3ba4db3cd
joaquimal12 <149969248+joaquimal12@users.noreply.github.com>
2026-06-27 14:08:34 +0100

    Delete navismart/dados_esp directory

--- navismart/dados_esp/src/comms.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "struct.h"
-
-// Encode and transmit processed data from Dados ESP to Display ESP.
-void comms_send(const MainData &m, const CompData &c);
-
-// Receive processed data on Display ESP.
-bool comms_receive(MainData &outMain, CompData &outComp);
